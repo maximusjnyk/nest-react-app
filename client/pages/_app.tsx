379d8f6ac253c2dcaf0a34de006a0890146d3a41
Maximus Janukovich <57828861+maximusjnyk@users.noreply.github.com>
2022-09-16 16:40:51 +0300

    add styles sass

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../app/assets/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from "../app/providers/AuthProvider";
+import '../app/assets/styles/old-styles.sass'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <AuthProvider>
